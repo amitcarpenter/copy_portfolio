ae976ea8ad1a82549b9c7d7e372d41f49de3c367
amitcarpenter <amitcarpenter199@gmail.com>
2024-05-04 01:25:12 +0530

    my potfolio work start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <h1 className='text-red-500'>hello</h1> */}
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App
