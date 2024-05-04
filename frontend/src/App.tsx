b2d220de34b7cc29c0291dc01ba5374c83b16165
amitcarpenter200 <amitcarpenter200@gmail.com>
2024-05-04 19:49:34 +0530

    hello

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
 import Header from "./components/Header/Header"
-import Footer from './components/Footer/Footer'
+// import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
       <Header />
       {/* <h1 className='text-red-500'>hello</h1> */}
-      <Footer />
+      {/* <Footer /> */}
       
     </>
   )
